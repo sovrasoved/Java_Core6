5fb2f583d607aa45e3cba4d9a7b77435c0cb5d0c
sovrasov <sovrasoved@list.ru>
2017-02-01 23:24:41 +0300

    Д-з 2 - full version hometask1 -  change  in       array.lenght        array of integer, double hometask2 - Continue with BANK example hometask3 -  withdraw money of particular account owner hometask4 - fund (пополнение) balance of particular user
    add draw1  f

--- module2/hometask1.java
+++ module2/hometask1.java
@@ -11,8 +11,6 @@ public class hometask1 {
         double[]dMas = {-10.4, -13.4, -45, 45, 12.3, 34.67, 51, -789.0, 34.6, 1.2};
 
 
-
-
         System.out.println("Summa of elements array_int = " + sum(iMas));
         System.out.println("Summa of elements array_double = " + sum(dMas));
         System.out.println("Min element of array_int = " + min(iMas));
@@ -36,137 +34,136 @@ public class hometask1 {
 
         System.out.println("Mutliplication of array_int = " + multiplication(iMas));
         System.out.println("Mutliplication of array_double = " + multiplication(dMas));
-        OutputArray(iMas);
+        outputArray(iMas);
         iNewMas = modulus(iMas);
-        OutputArray(iNewMas);
-        OutputArray(dMas);
+        outputArray(iNewMas);
+        outputArray(dMas);
         dNewMas = modulus(dMas);
-        OutputArray(dNewMas);
+        outputArray(dNewMas);
     }
 
 
-    private static void OutputArray(int array[]){
-        int iSize = array.length-1;
-        for (int i = 0; i < iSize ; i++) {
+    private static void outputArray(int array[]){
+
+        for (int i = 0; i < array.length ; i++) {
             System.out.print(array[i]+"  ");
         }
-        System.out.println(array[array.length-1]);
+        System.out.println(" ");
     }
-    private static void OutputArray(double array[]){
-        int iSize = array.length-1;
-        for (int i = 0; i < iSize ; i++) {
+    private static void outputArray(double array[]){
+
+        for (int i = 0; i < array.length ; i++)
             System.out.print(array[i]+"  ");
-        }
-        System.out.println(array[array.length-1]);
+
+        System.out.println(" ");
     }
     public static int sum(int array[]) {
-        int iSize = array.length;
+
         int isum = 0;
-        for (int i = 0; i < iSize; i++) {
+        for (int i = 0; i < array.length; i++)
             isum += array[i];
-        }
+
         return isum;
     }
     public static double sum(double array[]) {
-        int iSize = array.length;
+
         double dsum = 0;
-        for (int i = 0; i < iSize; i++) {
+        for (int i = 0; i < array.length; i++)
             dsum += array[i];
-        }
+
         return dsum;
     }
     public static int min(int array[]) {
-        int iSize = array.length;
+
         int imin = array[0];
-        for (int i = 1; i < iSize; i++) {
+        for (int i = 1; i < array.length; i++)
             if (imin > array[i]) imin = array[i];
-        }
+
         return imin;
     }
     public static double min(double array[]) {
-        int iSize = array.length;
+
         double dmin = array[0];
-        for (int i = 1; i < iSize; i++) {
+        for (int i = 1; i < array.length; i++)
             if (dmin > array[i]) dmin = array[i];
-        }
+
         return dmin;
     }
     public static int max(int array[]) {
-        int iSize = array.length;
+
         int imax = array[0];
-        for (int i = 1; i < iSize; i++) {
+        for (int i = 1; i < array.length; i++)
             if (imax < array[i]) imax = array[i];
-        }
+
         return imax;
     }
     public static double max(double array[]) {
-        int iSize = array.length;
+
         double dmax = array[0];
-        for (int i = 1; i < iSize; i++) {
+        for (int i = 1; i < array.length; i++)
             if (dmax < array[i]) dmax = array[i];
-        }
+
         return dmax;
     }
     public static int maxPositive(int array[]) {
         int imax = -1;
-        int iSize = array.length;
-        for (int i = 0; i < iSize; i++) {
+
+        for (int i = 0; i < array.length; i++)
             if (array[i] > 0 && imax < array[i]) imax = array[i];
-        }
+
         return imax;
     }
     public static double maxPositive(double array[]) {
         double dmax = -1;
-        int iSize = array.length;
-        for (int i = 0; i < iSize; i++) {
+
+        for (int i = 0; i < array.length; i++)
             if (array[i] > 0 && dmax < array[i]) dmax = array[i];
-        }
+
         return dmax;
     }
     public static int multiplication(int array[]) {
-        int iSize = array.length;
+
         int imul = array[0];
-        for (int i = 1; i < iSize; i++) {
+        for (int i = 1; i < array.length; i++)
             imul *= array[i];
-        }
+
         return imul;
     }
     public static double multiplication(double array[]) {
-        int iSize = array.length;
+
         double dmul = array[0];
-        for (int i = 1; i < iSize; i++) {
+        for (int i = 1; i < array.length; i++)
             dmul *= array[i];
-        }
+
         return dmul;
     }
     public static int[] modulus(int array[]) {
-        int iSize = array.length-1;
         array[0] = Math.abs(array[0]);
-        array[iSize] = Math.abs(array[iSize]);
+        array[array.length-1] = Math.abs(array[array.length-1]);
         return array;
     }
     public static double[] modulus(double array[]) {
-        int iSize = array.length-1;
+
         array[0] = Math.abs(array[0]);
-        array[iSize] = Math.abs(array[iSize]);
+        array[array.length-1] = Math.abs(array[array.length-1]);
         return array;
     }
     public static int secondLargest(int array[]) {
-        int iSize = array.length;
+
         int iMax = max(array);
         int iMaxLater = min(array);
-           for (int i = 0; i < iSize; i++) {
+           for (int i = 0; i < array.length; i++)
             if (iMax != array[i] & iMaxLater < array[i] ) iMaxLater = array[i];
-        }
+
         return iMaxLater;
     }
     public static double secondLargest(double array[]) {
-        int iSize = array.length;
+
         double dMax = max(array);
         double dMaxLater = min(array);
-               for (int i = 0; i < iSize; i++) {
+               for (int i = 0; i < array.length; i++)
             if (dMax != array[i] & dMaxLater < array[i] ) dMaxLater = array[i];
-        }
+
         return dMaxLater;
     }
 
